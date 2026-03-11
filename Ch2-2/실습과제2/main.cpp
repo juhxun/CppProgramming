0f30e0a7c0018768cbc288148487a7dcd45c86d6
juhyun <149346441+juhxun@users.noreply.github.com>
2026-03-11 10:30:38 +0900

    Create main.cpp

---
+++ Ch2-2/실습과제2/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main(void) {
+	int x;
+	
+	cout << "정수 x를 입력 하시오 : ";
+	cin >> x;
+
+	cout << "x=" << x << "일때 y의 값은" << x*x + 2*x -5 ;
+
+	return 0;
+} 
+
+int main() {
+
+	char num[10];
+	char name[10];
+	char adress[10];
+
+	cout << "학번을 입력하시오:";
+	cin >> num;
+	cout << "이름을 입력하시오:";
+	cin >> name; 
+	cout << "주소를 입력하시오:";
+	cin >> adress;
+	cout << endl;
+
+	cout << "1. 학번:" << num << endl;
+	cout << "2. 이름:" << name << endl;	
+	cout << "3. 주소:" << adress << endl;
+}*/
