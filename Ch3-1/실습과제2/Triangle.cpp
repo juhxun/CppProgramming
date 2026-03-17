3432d75aa19384df8820928de70d91b5dbbd533b
juhyun <149346441+juhxun@users.noreply.github.com>
2026-03-17 12:45:09 +0900

    Update Triangle.cpp

--- Ch3-1/실습과제2/Triangle.cpp
+++ Ch3-1/실습과제2/Triangle.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+class Triangle {
+public:
+    int x;
+    int y;
+    double getArea();
+};
+
+double Triangle::getArea() {
+    return 0.5 * x * y;
+}
+
+int main() {
+    Triangle tri;
+    tri.x = 3;
+    tri.y = 5;
+    cout << "삼각형의 면적은 " << tri.getArea() << endl;
+}
