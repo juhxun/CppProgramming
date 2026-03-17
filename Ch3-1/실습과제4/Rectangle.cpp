177424a9816a5a770974c74804e1d4dee9bbed6c
juhyun <149346441+juhxun@users.noreply.github.com>
2026-03-17 13:08:39 +0900

    Update Rectangle.cpp

--- Ch3-1/실습과제4/Rectangle.cpp
+++ Ch3-1/실습과제4/Rectangle.cpp
@@ -3,35 +3,28 @@
 // 날 짜 : 2026년 03월 17일
 // 작성자 : 2301406 박주현
 // **********************************************
-
 #include <iostream>
 using namespace std;
 
-class Rectangle { 
+class Rectangle {
 public:
-    int x;      
-    int y;      
-    int width;  
-    int height; 
+    int x = 1;
+    int y = 2;
+    int width = 3;
+    int height = 4;
 
-    Rectangle() { 
-        x = 1;
-        y = 2;
-        width = 3;
-        height = 4;
-    }
-
-    int getArea() { 
+    int getArea() {
         return width * height;
     }
 
-    int a() { 
+    int A() {
         return 2 * (width + height);
     }
 
-    void b(int& bx, int& by) { 
-        bx = x + width;
-        by = y - height;
+    void B() {
+        int bx = x + width;
+        int by = y - height;
+        cout << "우측 하단 좌표: (" << bx << "," << by << ")" << endl;
     }
 };
 
@@ -40,9 +33,7 @@ int main() {
 
     cout << "좌측 상단 좌표: (" << rect.x << "," << rect.y << ")" << endl;
     cout << "사각형의 면적: " << rect.getArea() << endl;
-    cout << "사각형의 둘레: " << rect.a() << endl;
+    cout << "사각형의 둘레: " << rect.A() << endl;
 
-    int bx, by;
-    rect.b(bx, by);
-    cout << "우측 하단 좌표: (" << bx << "," << by << ")" << endl;
+    rect.B();
 }
