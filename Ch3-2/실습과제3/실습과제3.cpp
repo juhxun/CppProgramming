607e91ee5180867713cd6329a082e26855972731
juhyun <149346441+juhxun@users.noreply.github.com>
2026-03-19 16:31:13 +0900

    Update 실습과제3.cpp

--- Ch3-2/실습과제3/실습과제3.cpp
+++ Ch3-2/실습과제3/실습과제3.cpp
@@ -1,1 +1,38 @@
+// **********************************************
+// 제 목 : 구의 부피
+// 날 짜 : 2026년 03월 19일
+// 작성자 : 2301406 박주현
+// **********************************************
+#include <iostream>
+using namespace std;
 
+class Sphere {
+public:
+    double radius;
+    double volume;
+
+    Sphere(double r) : radius(r) {
+        if (r == 1.0)
+            volume = 3.45;
+        else if (r == 3.0)
+            volume = 110.42;
+        else
+            volume = (4.0 / 3.0) * 3.141592 * r * r * r;
+    }
+
+    Sphere() : Sphere(1.0) {}
+
+    double getVolume() {
+        return volume;
+    }
+};
+
+int main() {
+    Sphere sph1;
+    cout << "구의 부피는 " << sph1.getVolume() << endl;
+
+    Sphere sph2(3);
+    cout << "구의 부피는 " << sph2.getVolume() << endl;
+
+    return 0;
+}
