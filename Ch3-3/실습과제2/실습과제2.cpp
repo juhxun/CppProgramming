590c1145ce7064dc806fb90b42a6b00a2fc13f96
juhyun <149346441+juhxun@users.noreply.github.com>
2026-03-23 13:31:26 +0900

    Update 실습과제2.cpp

--- Ch3-3/실습과제2/실습과제2.cpp
+++ Ch3-3/실습과제2/실습과제2.cpp
@@ -1,1 +1,34 @@
+// **********************************************
+// 제 목 : 삼각형 면적값 출력
+// 날 짜 : 2026년 03월 19일
+// 작성자 : 2301406 박주현
+// **********************************************
+#include <iostream>
 
+using namespace std;
+
+class Triangle {
+public:
+	double w, h;
+	double getArea();
+	Triangle(double _w, double _h);
+	Triangle();
+	~Triangle();
+};
+double Triangle::getArea() {
+	return w * h / 2;
+}
+Triangle::Triangle(double _w, double _h) : w(_w), h(_h) {
+	cout << "밑변 " << _w << "높이 " << _h << "인 삼각형 객체 생성" << endl;
+}
+Triangle::Triangle() : Triangle(1, 1) {}
+Triangle::~Triangle() {
+	cout << "밑변 " << w << "높이 " << h << "인 삼각형 객체 소멸" << endl;
+}
+int main(void) {
+	Triangle tri1;
+	cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+	Triangle tri2(2, 4);
+	cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+	return 0;
+}
