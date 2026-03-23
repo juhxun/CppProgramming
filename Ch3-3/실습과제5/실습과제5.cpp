1f327078d679e4c9e2154ed021e4f0f09dc4d70a
juhyun <149346441+juhxun@users.noreply.github.com>
2026-03-23 14:35:43 +0900

    Create 실습과제5.cpp

---
+++ Ch3-3/실습과제5/실습과제5.cpp
@@ -0,0 +1,37 @@
+// **********************************************
+// 제 목 : 삼각형 면적값 출력
+// 날 짜 : 2026년 03월 23일
+// 작성자 : 2301406 박주현
+// **********************************************
+#include <iostream>
+using namespace std;
+
+class Sphere {
+private:
+    double r;
+
+public:
+    Sphere();
+    Sphere(double _r);
+    ~Sphere();
+    double getVolume();
+};
+double Sphere::getVolume() {
+    return 4.0 / 3.0 * 3.14 * r * r * r;
+}
+Sphere::Sphere(double _r) : r(_r) {
+    cout << "반지름 " << r << "인 구 생성" << endl;
+}
+Sphere::Sphere() : Sphere(1) {}
+
+Sphere::~Sphere() {
+    cout << "반지름 " << r << "인 구 소멸" << endl;
+}
+Sphere sph1(10);
+Sphere sph2(20);
+
+int main() {
+    cout << "구의 부피는 " << sph1.getVolume() << endl;
+    cout << "구의 부피는 " << sph2.getVolume() << endl;
+    return 0;
+}
