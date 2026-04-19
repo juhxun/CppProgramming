2125fb9448875bcea5d4caa9f0b7e0d61bdad93a
juhyun <149346441+juhxun@users.noreply.github.com>
2026-04-19 21:33:44 +0900

    Create 실습과제3.cpp

---
+++ Ch4-4/실습과제3/실습과제3.cpp
@@ -0,0 +1,26 @@
+// **********************************************
+// 제 목 : 실습과제3
+// 날 짜 : 2026년 4월 14일
+// 작성자 : 2301406 박주현
+// **********************************************
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	int count = 0;
+	cout << "문자열을 입력: ";
+	getline(cin, str);
+	for (int i = 0; i < str.length(); i++)
+	{
+		if(str[i] == 'a')
+		{
+			count++;
+		}
+	}
+	cout << "문자 a는 " << count << "개 있습니다." << endl;
+	return 0;
+}
