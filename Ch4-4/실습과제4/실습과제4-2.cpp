eba05fa16a6465eb07d7f1e4a3abfbc944f5defe
juhyun <149346441+juhxun@users.noreply.github.com>
2026-04-20 01:34:34 +0900

    Update 실습과제4-2.cpp
    Add functionality to filter non-alphabetic characters from input text.

--- Ch4-4/실습과제4/실습과제4-2.cpp
+++ Ch4-4/실습과제4/실습과제4-2.cpp
@@ -1,1 +1,23 @@
+// **********************************************
+// 제 목: 실습과제 4-1(4번)
+// 날 짜: 2026년 4월 14일
+// 작성자: 2301406 박주현
+// **********************************************
+#include <iostream>
+#include <string>
 
+using namespace std;
+int main(void) {
+	string text;
+	string ntext;
+	cout << "텍스트 입력(한글 안 됨)>>";
+	getline(cin, text, '\n');
+
+	for (int i = 0; i < text.length(); i++) {
+		if (isalpha(text[i]) || text[i] == ' ') {
+			ntext += text[i];
+		}
+	}
+	cout << ntext;
+	return 0;
+}
