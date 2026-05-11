b55771ed8d2c7b67cd334cf12c7169bdbb67964e
juhyun <149346441+juhxun@users.noreply.github.com>
2026-05-11 19:28:50 +0900

    Create 실습과제4.cpp
    Implement Circle class with methods to modify radius and retrieve its value.

---
+++ Ch5-3/실습과제4/실습과제4.cpp
@@ -0,0 +1,42 @@
+// **********************************************
+// 제 목 : 실습과제 4
+// 날 짜 : 2026년 05월 11일
+// 작성자 : 2301406 박주현
+// **********************************************
+
+#include <iostream>
+using namespace std;
+
+class Circle {
+    int radius;
+
+public:
+    Circle(int radius) {
+        this->radius = radius;
+    }
+
+    Circle& plus(int n) {
+        radius += n;
+        return *this;
+    }
+
+    Circle& minus(int n) {
+        radius -= n;
+        return *this;
+    }
+
+    int getRadius() {
+        return radius;
+    }
+};
+
+int main() {
+    Circle a(5);
+
+    a.plus(1).plus(2).plus(3)
+        .minus(3).minus(2).minus(1).minus(5);
+
+    cout << "객체 a의 반지름은 " << a.getRadius();
+
+    return 0;
+}
