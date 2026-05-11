9d34170b8a27bd2e226a02419c53ea1ae1cad9a1
juhyun <149346441+juhxun@users.noreply.github.com>
2026-05-11 19:25:01 +0900

    Create 실습과제3.cpp

---
+++ Ch5-3/실습과제3/실습과제3.cpp
@@ -0,0 +1,32 @@
+// **********************************************
+// 제 목 : 실습과제 2
+// 날 짜 : 2026년 05월 11일
+// 작성자 : 2301406 박주현
+// **********************************************
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void SwapString(string& a, string& b);
+
+int main() {
+    string s1("hello");
+    string s2("world");
+
+    cout << "교환전 문자열 " << s1 << " " << s2 << endl;
+
+    SwapString(s1, s2);
+
+    cout << "교환후 문자열 " << s1 << " " << s2 << endl;
+
+    return 0;
+}
+
+void SwapString(string& a, string& b) {
+    string t;
+
+    t = a;
+    a = b;
+    b = t;
+}
