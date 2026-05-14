a9abaa0a568d36278325d31d3a18cc8779286b25
juhyun <149346441+juhxun@users.noreply.github.com>
2026-05-15 04:55:11 +0900

    Update 실습과제3.cpp

--- Ch5-4/실습과제3/실습과제3.cpp
+++ Ch5-4/실습과제3/실습과제3.cpp
@@ -3,68 +3,77 @@
 // 날 짜 : 2026년 05월14일
 // 작성자 : 2301406 박주현
 // **********************************************
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Mystack {
-	int* p;
-	int size;
-	int tos;
+class Collector {
+    int* p;
+    int size = 0;
+
 public:
-	Mystack();
-	Mystack(int size);
-	Mystack(const Mystack& src);
-	~Mystack();
-	bool push(int n);
-	bool pop(int& n);
+    Collector(int size, int values[]) {
+        this->size = size;
 
+        p = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            p[i] = values[i];
+        }
+    }
+
+    Collector(const Collector& src) {
+        size = src.size;
+
+        p = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            p[i] = src.p[i];
+        }
+    }
+
+    ~Collector() {
+        delete[] p;
+    }
+
+    void show() {
+        cout << "데이터 수 " << size << ": ";
+
+        for (int i = 0; i < size; i++) {
+            cout << p[i] << " ";
+        }
+
+        cout << endl;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    int get(int index) {
+        return p[index];
+    }
 };
-Mystack::Mystack() :Mystack(10) {}
-Mystack::Mystack(int size) {
-	this->size = size;
-	this->tos = 0;
-	p = new int[size];
-}
-Mystack::Mystack(const Mystack& src) {
-	this->size = src.size;
-	this->tos = src.tos;
 
-	p = new int[size];
+double calcAvg(Collector c) {
+    int sum = 0;
 
-	for (int i = 0; i < tos; i++) {
-		p[i] = src.p[i];
-	}
-}
-Mystack::~Mystack() {
-	delete[]p;
-}
-bool Mystack::push(int n) {
-	if (size <= tos) return false;
-	else {
-		p[tos++] = n;
-		return true;
-	}
-}
-bool Mystack::pop(int& n) {
-	if (tos == 0) return false;
-	else {
-		n = p[--tos];
-		return true;
-	}
+    for (int i = 0; i < c.getSize(); i++) {
+        sum += c.get(i);
+    }
+
+    return (double)sum / c.getSize();
 }
 
 int main() {
-	Mystack a(10);
-	a.push(10);
-	a.push(20);
+    int temp[] = { 69, 70, 71, 72, 74 };
 
-	Mystack b = a;
-	b.push(30);
-	int n;
-	a.pop(n);
-	cout << "스택 a에서 팝한 값 " << n << endl;	//20
-	b.pop(n);
-	cout << "스택 b에서 팝한 값 " << n << endl;	//30
+    Collector weight(4, temp);
+
+    double avg = calcAvg(weight);
+
+    weight.show();
+
+    cout << "평균은 " << avg << endl;
+
+    return 0;
 }
