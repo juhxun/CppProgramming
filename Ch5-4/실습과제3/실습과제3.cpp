0fe596ca9822a88b150cae2034bf37337bca334f
juhyun <149346441+juhxun@users.noreply.github.com>
2026-05-15 04:04:12 +0900

    Update 실습과제3.cpp

--- Ch5-4/실습과제3/실습과제3.cpp
+++ Ch5-4/실습과제3/실습과제3.cpp
@@ -3,78 +3,68 @@
 // 날 짜 : 2026년 05월14일
 // 작성자 : 2301406 박주현
 // **********************************************
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
 using namespace std;
 
-class MyStack {
-    int* p;
-    int size = 0;
-    int tos = 0;
-
+class Mystack {
+	int* p;
+	int size;
+	int tos;
 public:
-    MyStack() {
-        size = 10;
-        p = new int[size];
-    }
+	Mystack();
+	Mystack(int size);
+	Mystack(const Mystack& src);
+	~Mystack();
+	bool push(int n);
+	bool pop(int& n);
 
-    MyStack(int size) {
-        this->size = size;
-        p = new int[size];
-    }
-
-    MyStack(const MyStack& src) {
-        size = src.size;
-        tos = src.tos;
-
-        p = new int[size];
-
-        for (int i = 0; i < tos; i++) {
-            p[i] = src.p[i];
-        }
-    }
-
-    ~MyStack() {
-        delete[] p;
-    }
-
-    bool push(int n) {
-        if (tos == size)
-            return false;
-
-        p[tos] = n;
-        tos++;
-
-        return true;
-    }
-
-    bool pop(int& n) {
-        if (tos == 0)
-            return false;
-
-        tos--;
-        n = p[tos];
-
-        return true;
-    }
 };
+Mystack::Mystack() :Mystack(10) {}
+Mystack::Mystack(int size) {
+	this->size = size;
+	this->tos = 0;
+	p = new int[size];
+}
+Mystack::Mystack(const Mystack& src) {
+	this->size = src.size;
+	this->tos = src.tos;
+
+	p = new int[size];
+
+	for (int i = 0; i < tos; i++) {
+		p[i] = src.p[i];
+	}
+}
+Mystack::~Mystack() {
+	delete[]p;
+}
+bool Mystack::push(int n) {
+	if (size <= tos) return false;
+	else {
+		p[tos++] = n;
+		return true;
+	}
+}
+bool Mystack::pop(int& n) {
+	if (tos == 0) return false;
+	else {
+		n = p[--tos];
+		return true;
+	}
+}
 
 int main() {
-    MyStack a(10);
+	Mystack a(10);
+	a.push(10);
+	a.push(20);
 
-    a.push(10);
-    a.push(20);
-
-    MyStack b = a;
-
-    b.push(30);
-
-    int n;
-
-    a.pop(n);
-    cout << "스택 a에서 팝한 값 " << n << endl;
-
-    b.pop(n);
-    cout << "스택 b에서 팝한 값 " << n << endl;
-
-    return 0;
+	Mystack b = a;
+	b.push(30);
+	int n;
+	a.pop(n);
+	cout << "스택 a에서 팝한 값 " << n << endl;	//20
+	b.pop(n);
+	cout << "스택 b에서 팝한 값 " << n << endl;	//30
 }
