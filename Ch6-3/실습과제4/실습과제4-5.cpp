1f39267904fc51f4605d00a37f2cc3b9416ca936
juhyun <149346441+juhxun@users.noreply.github.com>
2026-05-22 23:28:14 +0900

    Create 실습과제4-5.cpp
    Implement Vector class with constructors and show methods.

---
+++ Ch6-3/실습과제4/실습과제4-5.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Vector {
+    int* mem;
+    int size;
+
+public:
+    Vector() {
+        size = 5;
+        mem = new int[size];
+
+        for (int i = 0; i < size; i++)
+            mem[i] = 0;
+    }
+
+    Vector(int s, int value) {
+        size = s;
+        mem = new int[size];
+
+        for (int i = 0; i < size; i++)
+            mem[i] = value;
+    }
+    ~Vector() {
+        delete[] mem;
+    }
+
+    void show() {
+        for (int i = 0; i < size; i++)
+            cout << mem[i] << " ";
+
+        cout << endl;
+    }
+
+    void show(int n) {
+        for (int i = 0; i < n; i++)
+            cout << mem[i] << " ";
+
+        cout << endl;
+    }
+};
+
+int main() {
+    Vector x;
+    Vector y(10, 8);
+
+    x.show();
+    y.show();
+    y.show(3);
+
+    return 0;
+}
