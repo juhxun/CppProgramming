c982b355839f45dee08002f3759de9c52a1c92f4
juhyun <149346441+juhxun@users.noreply.github.com>
2026-06-08 10:28:07 +0900

    Update 실습과제2.cpp

--- Ch7-3/실습과제2/실습과제2.cpp
+++ Ch7-3/실습과제2/실습과제2.cpp
@@ -3,3 +3,53 @@
 // 날 짜 : 2026년 06월08일
 // 작성자 : 2301406 박주현
 // **********************************************
+
+#include <iostream>
+using namespace std;
+
+class Complex {
+    int real;
+    int img;
+
+public:
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        img = i;
+    }
+
+    void show() {
+        cout << real << "+" << img << "j" << endl;
+    }
+
+    friend Complex& operator++(Complex& c);
+    friend Complex operator--(Complex& c, int);
+};
+
+Complex& operator++(Complex& c) {
+    c.real++;
+    c.img++;
+    return c;
+}
+
+Complex operator--(Complex& c, int) {
+    Complex temp = c;
+
+    c.real--;
+    c.img--;
+
+    return temp;
+}
+
+int main() {
+    Complex x(2, -3);
+
+    ++x;
+    cout << "증가결과 ";
+    x.show();
+
+    x--;
+    cout << "감소결과 ";
+    x.show();
+
+    return 0;
+}
